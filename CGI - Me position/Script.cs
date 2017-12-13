51fc5ea034e05b1af8685ebc41332826e0f3768f
Wompi <robocode@casepool.de>
2017-12-13 03:10:57 -0800

    Initial commit - just a lol script to echo the current position of a programmable block (just to have it handy if needed) I used it mostly for distance measurments

---
+++ CGI - Me position/Script.cs
@@ -0,0 +1,35 @@
+﻿public Program() { 
+    // The constructor, called only once every session and
+    // always before any other method is called. Use it to
+    // initialize your script. 
+    //     
+    // The constructor is optional and can be removed if not
+    // needed. 
+} 
+ 
+public void Save() { 
+    // Called when the program needs to save its state. Use
+    // this method to save your state to the Storage field
+    // or some other means. 
+    // 
+    // This method is optional and can be removed if not
+    // needed. 
+} 
+ 
+public void Main(string argument) 
+{ 
+    string aOutput = "";
+
+    string aPosition = Me.GetPosition().ToString();
+    Echo(aPosition);
+    aOutput = aOutput + aPosition.ToString() + "\n";
+
+
+    Quaternion aQuaternion = new Quaternion(0,0,0,0);
+    Me.Orientation.GetQuaternion(out aQuaternion);
+    Echo(aQuaternion.ToString());
+    aOutput = aOutput + aQuaternion.ToString() + "\n";
+
+
+    Me.CustomData = aOutput;; 
+} 
