04841459d335aeb5554ad5cf33a6c6175fd6eed2
Robert Schott <games@casepool.de>
2017-12-25 19:14:33 +0100

    Collect all Blocks and sort them to the variables and lists .. als debug to see if all works well

--- CGI - Kowari Control/Script.cs
+++ CGI - Kowari Control/Script.cs
@@ -4,16 +4,16 @@
  *
  *    Version:
  *              v0.10   - all the good stuff to make live easier on a small welding ship
- *                          - batteries will be recharge if connected to another grid 
+ *                          - batteries will be recharge if connected to another grid
  *                          - tool will be switched of if jumping out of the cockpit
- *                          - reactor switched of when connected to another grid 
+ *                          - reactor switched of when connected to another grid
  *              v0.12   - only grab the blocks on the grid - nice adjustment to the 'GetFirstBlockOfType' generic
  *
  *
  */
 
 IMyShipConnector myShipConnector = null;
-IMyLandingGear myLandingGear = null; 
+IMyLandingGear myLandingGear = null;
 IMyReactor myReactor = null;
 IMyGyro myGyro = null;
 IMyCockpit myCockpit = null;
@@ -27,68 +27,64 @@ IMyRadioAntenna myAntenna = null;
 
 List<IMyBatteryBlock> myBatteries = new List<IMyBatteryBlock>();
 List<IMySolarPanel> mySolarPanels = new List<IMySolarPanel>();
-List<IMyTextPanel> myTextPanels = new List<IMyTextPanel>(); 
+List<IMyTextPanel> myTextPanels = new List<IMyTextPanel>();
 
 
-static double ANTENNA_ENERGY_FACTOR = 0.004;    // NOTE: could be changed in future versions for now the energy input is linear (4W) 
+static double ANTENNA_ENERGY_FACTOR = 0.004;    // NOTE: could be changed in future versions for now the energy input is linear (4W)
 
 int mTick = 0;
 
 public void GetFirstBlockOfType<T>( ref T pBlock, Func<IMyTerminalBlock, bool> pCheck = null ) where T : class
 {
     List<T> aList = new List<T>();
-    GridTerminalSystem.GetBlocksOfType<T>(aList);
-
-    //T aResult = null;
+    GridTerminalSystem.GetBlocksOfType<T>(aList, pCheck);
     if (aList.Count > 0) pBlock = aList[0];
-
-    //return aResult;
 }
 
-public Program() 
-{ 
+public Program()
+{
     Runtime.UpdateFrequency  = UpdateFrequency.Update10;
 
     Func<IMyTerminalBlock, bool> aCheck = b => b.CubeGrid == Me.CubeGrid;
 
     GetFirstBlockOfType(ref myShipConnector, aCheck);
     GetFirstBlockOfType(ref myLandingGear, aCheck);
-    GetFirstBlockOfType(ref myReactor, aCheck); 
-    GetFirstBlockOfType(ref myGyro, aCheck); 
-    GetFirstBlockOfType(ref myCockpit, aCheck);  
+    GetFirstBlockOfType(ref myReactor, aCheck);
+    GetFirstBlockOfType(ref myGyro, aCheck);
+    GetFirstBlockOfType(ref myCockpit, aCheck);
     GetFirstBlockOfType(ref myRemoteControl, aCheck);
-    GetFirstBlockOfType(ref myGasGenerator, aCheck); 
-    //GetFirstBlockOfType(ref myWelder, aCheck);  
-    //GetFirstBlockOfType(ref myGrinder, aCheck); 
+    GetFirstBlockOfType(ref myGasGenerator, aCheck);
+    //GetFirstBlockOfType(ref myWelder, aCheck);
+    //GetFirstBlockOfType(ref myGrinder, aCheck);
     GetFirstBlockOfType(ref myTool, aCheck);
-    GetFirstBlockOfType(ref myAntenna, aCheck);   
-      
-    
-    GridTerminalSystem.GetBlocksOfType(myBatteries, aCheck); 
-    GridTerminalSystem.GetBlocksOfType(mySolarPanels, aCheck);  
-    GridTerminalSystem.GetBlocksOfType(myTextPanels, aCheck);  
+    GetFirstBlockOfType(ref myAntenna, aCheck);
+
+
+    GridTerminalSystem.GetBlocksOfType(myBatteries, aCheck);
+    GridTerminalSystem.GetBlocksOfType(mySolarPanels, aCheck);
+    GridTerminalSystem.GetBlocksOfType(myTextPanels, aCheck);
 
 
     foreach( IMyTextPanel aPanel in myTextPanels)
     {
-        aPanel.FontSize = 1f; 
-        aPanel.Font = "Monospace"; 
+        aPanel.FontSize = 1f;
+        aPanel.Font = "Monospace";
         aPanel.ShowPublicTextOnScreen();
     }
-} 
- 
-public void Save() {} 
- 
-public void Main(string argument, UpdateType updateSource) 
-{ 
+}
+
+public void Save() {}
+
+public void Main(string argument, UpdateType updateSource)
+{
     mTick++;
 
- 
+
     Echo(myShipConnector.Status.ToString());
     Echo(myLandingGear.LockMode.ToString());
 
-  
-    Echo("Batteries: "+myBatteries.Count); 
+
+    Echo("Batteries: "+myBatteries.Count);
     foreach ( IMyBatteryBlock aBattery in myBatteries)
     {
         string isRecharge = aBattery.OnlyRecharge ? "Y" : "N";
@@ -114,13 +110,13 @@ public void Main(string argument, UpdateType updateSource)
     Echo("Reactor: "+myReactor.CurrentOutput);
     bool aReactorState = HandleReactor();
     Echo(aReactorState ? "Connected: reactor offline" : "Not Connected: reactor online");
-    
+
     double aSolarOutput = 0;
     foreach ( IMySolarPanel aPanel in mySolarPanels)
     {
-        aSolarOutput += aPanel.CurrentOutput; 
+        aSolarOutput += aPanel.CurrentOutput;
     }
- 
+
     Echo("Solar: "+mySolarPanels.Count + " - "+aSolarOutput.ToString("0.000"));
 
 
@@ -130,12 +126,12 @@ public void Main(string argument, UpdateType updateSource)
 
 
 
-    double aRadius =  myAntenna.Radius; 
+    double aRadius =  myAntenna.Radius;
     Echo("Antenna: "+aRadius.ToString("0.00")+ " Input: "+(aRadius * ANTENNA_ENERGY_FACTOR).ToString("0.000"));
 
 
     string aIndicator = myCockpit.MoveIndicator.ToString("00.00");
-    
+
 
 
     string aOutput = "\uE120";
@@ -150,9 +146,9 @@ public void Main(string argument, UpdateType updateSource)
     Vector3D aCenterOfMass = myCockpit.CenterOfMass;
     Vector3D aVolumeCenter  = Me.CubeGrid.WorldVolume.Center;
     double aVolumeRadius = Me.CubeGrid.WorldVolume.Radius;
-    double aSize = Me.CubeGrid.GridSize;     
+    double aSize = Me.CubeGrid.GridSize;
 
-   
+
     double  aDistance = Vector3D.Distance(aVolumeCenter,aCenterOfMass);
 
     aOutput += String.Format("Grid Volume:\n Center: {0}\n Radius: {1}\n Size: {2}\n Blocks: {3}\n",
@@ -166,13 +162,13 @@ public void Main(string argument, UpdateType updateSource)
     Vector3D aLinearVelocity = myCockpit.GetShipVelocities().LinearVelocity;
     double aSpeed = aLinearVelocity.Length();
 
-    Vector3D aUp = myCockpit.WorldMatrix.Up; 
-    Vector3D aLeft = myCockpit.WorldMatrix.Left;   
-    Vector3D aForward = myCockpit.WorldMatrix.Forward;  
+    Vector3D aUp = myCockpit.WorldMatrix.Up;
+    Vector3D aLeft = myCockpit.WorldMatrix.Left;
+    Vector3D aForward = myCockpit.WorldMatrix.Forward;
 
     double aUpVelo = aLinearVelocity.Dot(aUp);
-    double aForwardVelo = aLinearVelocity.Dot(aForward); 
-    double aLeftVelo = aLinearVelocity.Dot(aLeft);  
+    double aForwardVelo = aLinearVelocity.Dot(aForward);
+    double aLeftVelo = aLinearVelocity.Dot(aLeft);
 
 
    // double aBreakDistanceUp = -aSpeed / (2 * )
@@ -186,7 +182,7 @@ public void Main(string argument, UpdateType updateSource)
 
 
     myTextPanels[0].WritePublicText(aOutput,false);
-} 
+}
 
 /**
   *  Note: for now set the batteries to recharge when we are connected
@@ -201,28 +197,28 @@ public bool HandleBatteries()
     if (myShipConnector.Status == MyShipConnectorStatus.Connected)
     {
         isRecharge = true;
-        aResult = true; 
+        aResult = true;
     }
-    
-     foreach( IMyBatteryBlock aBattery in myBatteries) 
-     { 
-        aBattery.OnlyRecharge = isRecharge; 
-     }    
+
+     foreach( IMyBatteryBlock aBattery in myBatteries)
+     {
+        aBattery.OnlyRecharge = isRecharge;
+     }
 
     return aResult;
 }
 
 public bool HandleReactor()
 {
-    bool aResult = false; 
- 
+    bool aResult = false;
+
     string aAction = "OnOff_On";
-    if (myShipConnector.Status == MyShipConnectorStatus.Connected) 
-    { 
-        aAction = "OnOff_Off"; 
-        aResult = true;  
-    } 
-     
+    if (myShipConnector.Status == MyShipConnectorStatus.Connected)
+    {
+        aAction = "OnOff_Off";
+        aResult = true;
+    }
+
     myReactor.ApplyAction(aAction);
     return aResult;
 }
@@ -230,15 +226,15 @@ public bool HandleReactor()
 /**
   *    TODO: find a better way to check which tool is equipped
   *                 - a tool can be changed by grinding it down and replace it
-  *                 - also a tool can be damaged 
+  *                 - also a tool can be damaged
   *
   *     TODO: implement a remote control check
   *                 - a ship can be active remote controlled or be controlled by the autopilot
-  * 
+  *
   *     TODO: handle the return result
   */
 public bool HandleTool()
-{   
+{
     bool aResult = false;
 
     if (myTool == null || !myTool.IsFunctional)
@@ -248,12 +244,12 @@ public bool HandleTool()
     }
     else
     {
-        if (!myCockpit.IsUnderControl)  
-        {  
-            if (myTool.Enabled) 
-            { 
-                myTool.ApplyAction("OnOff_Off"); 
-            }       
+        if (!myCockpit.IsUnderControl)
+        {
+            if (myTool.Enabled)
+            {
+                myTool.ApplyAction("OnOff_Off");
+            }
         }
         aResult = true;
     }
