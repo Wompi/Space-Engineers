64338c8fe16c0e94ac2dcf573e65882f699c8d9a
Wompi <robocode@casepool.de>
2017-12-30 09:28:16 -0800

    bugfix

--- CGI - Kowari Control/Script.cs
+++ CGI - Kowari Control/Script.cs
@@ -26,7 +26,7 @@ IMyRadioAntenna myAntenna = null;
 
 List<IMyBatteryBlock> myBatteries = new List<IMyBatteryBlock>();
 List<IMySolarPanel> mySolarPanels = new List<IMySolarPanel>();
-List<IMyTextPanel> myTextPanels = new List<IMyTextPanel>();
+List<IMyTextPanel> myLCDPanels = new List<IMyTextPanel>();
 
 
 const double ANTENNA_ENERGY_FACTOR = 0.004;    // NOTE: could be changed in future versions for now the energy input is linear (4W)
@@ -63,10 +63,10 @@ public Program()
 
     GridTerminalSystem.GetBlocksOfType(myBatteries, aCheck);
     GridTerminalSystem.GetBlocksOfType(mySolarPanels, aCheck);
-    GridTerminalSystem.GetBlocksOfType(myTextPanels, aCheck);
+    GridTerminalSystem.GetBlocksOfType(myLCDPanels, aCheck);
 
 
-    foreach( IMyTextPanel aPanel in myTextPanels)
+    foreach( IMyTextPanel aPanel in myLCDPanels)
     {
         aPanel.FontSize = 1f;
         aPanel.Font = "Monospace";
@@ -175,13 +175,12 @@ public string HandleReactor(bool pIsConnected)
     if (pIsConnected)
     {
         aAction = "OnOff_Off";
-        aResult = true;
     }
     myReactor.ApplyAction(aAction);
     string aStatusString = myReactor.Enabled ? C_GREEN + " (on)" : C_RED + " (off)";
 
     aResult += String.Format("Reactor: {0} - {1:0.000} kw\n",
-                aStatusString
+                aStatusString,
                 myReactor.CurrentOutput);
 
     return aResult;
@@ -218,7 +217,7 @@ public string HandleTool(bool pIsConnected)
         }
         else
         {
-            aStats = myTool.Enabled ? C_GREEN : C_YELLOW;
+            aStatus = myTool.Enabled ? C_GREEN : C_YELLOW;
         }
     }
     aResult += String.Format("Tool: {0} {1}\n",aStatus,myTool.CustomName);
@@ -263,7 +262,7 @@ public string HandleAntenna(bool pIsConnected)
         myAntenna.Enabled = true;
         aStatus = C_GREEN;
     }
-    aResult += String.Format("Antenna: {0} {1} m  {2} kw\n",
+    aResult += String.Format("Antenna: {0} {1:0} m  {2:0.000} kw\n",
                 aStatus,
                 aRadius,
                 (aRadius * ANTENNA_ENERGY_FACTOR));
