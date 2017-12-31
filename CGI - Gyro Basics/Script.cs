bcbcf5c6d1c25df112bdc94b37375342620432c1
Robert Schott <games@casepool.de>
2017-12-31 09:32:57 +0100

    start with the alignment code to the gravity vector

--- CGI - Gyro Basics/Script.cs
+++ CGI - Gyro Basics/Script.cs
@@ -3,21 +3,23 @@
  *      Program:  CGI - Gyro Bsics
  *
  *      Version:
- *              v0.10 - just the basic functions for a gyro block - also maybe a gyro test routine to check the angle acceleration 
+ *              v0.10 - just the basic functions for a gyro block - also maybe a gyro test routine to check the angle acceleration
  *
  */
 
 CGI_GyroManager myGyroManager = new CGI_GyroManager();
 
-IMyCockpit myCockpit = null;
+IMyShipController myCockpit = null;
+List<IMyTextPanel> myLCDPanels = new List<IMyTextPanel>();
 
-public Program() 
+public Program()
 {
     Runtime.UpdateFrequency = UpdateFrequency.Update10;
-    List<IMyCockpit> aList = new List<IMyCockpit>();  
-    GridTerminalSystem.GetBlocksOfType(aList);  
-    myCockpit = aList[0]; 
+    List<IMyShipController> aList = new List<IMyShipController>();
+    GridTerminalSystem.GetBlocksOfType(aList, b => b.CubeGrid == Me.CubeGrid);
+    myCockpit = aList[0];
 
+    GridTerminalSystem.GetBlocksOfType(myLCDPanels, b => b.CubeGrid == Me.CubeGrid);
 
     myGyroManager.LoadEntities(GridTerminalSystem,myCockpit);
 
@@ -31,47 +33,83 @@ public string mArgument = "";
 
 public void Main(string argument, UpdateType updateSource)
 {
-      string aOut = "";
+    string aOut = "";
 
-      if (argument.Equals("GyroCheck"))
-      {
-            mArgument = argument;
-      }
+    if (argument.Equals("GyroCheck"))
+    {
+        mArgument = argument;
+    }
 
+    aOut = myGyroManager.Statistics();
 
+    if (mArgument.Equals("GyroCheck"))
+    {
+        aOut += myGyroManager.ProcessGyroCheck();
+    }
 
+    aOut += myGyroManager.ProcessGravityAlignment();
 
-      aOut = myGyroManager.Statistics();
-
-      if (mArgument.Equals("GyroCheck"))
-      {
-            aOut = aOut + myGyroManager.ProcessGyroCheck();
-      }
-
-
-      Echo(aOut); 
+    if (myTextPanels.Count > 0)
+    {
+    myTextPanels[0].WritePublicText(aOut,false);
+    }
+    else
+    {
+        Echo(aOut);
+    }
 }
 
 
 public class CGI_GyroManager
 {
     private List<IMyGyro> mGyros = new List<IMyGyro>();
-    private IMyCockpit mReference = null;
+    private IMyShipController mReference = null;
 
     private bool isStart = true;
     private Vector3D aCheckStartPosition = Vector3D.Zero;
-    private float POWER_CHECK_DELTA = 0.001f; 
+    private float POWER_CHECK_DELTA = 0.001f;
 
 
 
 
-    public string LoadEntities(IMyGridTerminalSystem pGTS, IMyCockpit pReference)
+    public string LoadEntities(IMyGridTerminalSystem pGTS, IMyShipController pReference)
     {
         string aOut = "";
         pGTS.GetBlocksOfType(mGyros);
         mReference = pReference;
         return aOut;
     }
+
+    public string ProcessGravityAlignment()
+    {
+        string aResult = "";
+        Vector3D aWorldGravity = mReference.GetNaturalGravity();
+
+        Vector3D aUp = mReference.WorldMatrix.Up;
+        Vector3D aLeft = mReference.WorldMatrix.Left;
+        Vector3D aForward = mReference.WorldMatrix.Forward;
+
+        Vector3D aLocalGravity = Vector3D.Zero;
+
+        aLocalGravity.X = aWorldGravity.Dot(aUp);
+        aLocalGravity.Y = aWorldGravity.Dot(aLeft);
+        aLocalGravity.Z = aWorldGravity.Dot(aFoward);
+
+        Vector3D aRadianAngleVector = Vector3D.Zero;
+        aRadianAngleVector.X = Math.Acos(aLocalGravity.X / (aWorldGravity.Length() * aUp.Length())); // YAW (not needed)
+        aRadianAngleVector.Y = Math.Acos(aLocalGravity.Y / (aWorldGravity.Length() * aLeft.Length())); // ROLL
+        aRadianAngleVector.Z = Math.Acos(aLocalGravity.Z / (aWorldGravity.Length() * aForward.Length())); // PITCH
+
+        Vector3D aDegreeAngleVector = aRadianAngleVector * 180.00f / Math.PI;
+
+        aResult += String.Format("Angle: Gravity -> Ship\n Pitch: {0:00.00}\n Roll: {1:00.00}\n Yaw: {2:00.00}\n",
+                        aRadianAngleVector.Z,
+                        aLocalGravity.Y,
+                        aLocalGravity.Z);
+        return aResult;
+    }
+
+
 
     public string ProcessGyroCheck()
     {
@@ -92,26 +130,26 @@ public class CGI_GyroManager
         }
         else
         {
-            Vector3D aCheckVector = mGyros[0].WorldMatrix.Forward + Vector3D.Forward; 
+            Vector3D aCheckVector = mGyros[0].WorldMatrix.Forward + Vector3D.Forward;
 
             if (aCheckVector.Equals(aCheckStartPosition))
             {
                  aOut = aOut + "Run\n";
-                 
-                 foreach ( IMyGyro aGyro in mGyros) 
-                 { 
-                      aGyro.GyroPower += POWER_CHECK_DELTA ;   
-                      aGyro.Yaw = 6.283f;
+
+                 foreach ( IMyGyro aGyro in mGyros)
+                 {
+                      aGyro.GyroPower += POWER_CHECK_DELTA ;
+                      aGyro.Yaw = 6.283f; // pi * 2
                       aOut = aOut + String.Format("{0} - power: {1:000.000}\n",aGyro.CustomName,aGyro.GyroPower);
-    
+
                  }
             }
             else
             {
-                 foreach ( IMyGyro aGyro in mGyros)  
-                 {  
+                 foreach ( IMyGyro aGyro in mGyros)
+                 {
                       aGyro.GyroOverride = false;
-                      aOut = aOut + String.Format("Complete {0} - power: {1:000.000}\n",aGyro.CustomName,aGyro.GyroPower); 
+                      aOut = aOut + String.Format("Complete {0} - power: {1:000.000}\n",aGyro.CustomName,aGyro.GyroPower);
                  }
             }
         }
@@ -121,15 +159,15 @@ public class CGI_GyroManager
     public string Statistics()
     {
         string aOut = "";
-        aOut = aOut + String.Format("Gyros: {0}\n\n",mGyros.Count); 
-        
+        aOut = aOut + String.Format("Gyros: {0}\n\n",mGyros.Count);
+
         Vector3D aForwardReference = mReference.WorldMatrix.Forward;
 
         foreach( IMyGyro aGyro in mGyros)
         {
-                string aAlign = "not aligned"; 
+                string aAlign = "not aligned";
                 Vector3D aGyroForward = aGyro.WorldMatrix.Forward;
-        
+
                 if (aForwardReference.Equals(aGyroForward))
                 {
                         aAlign = "aligned";
